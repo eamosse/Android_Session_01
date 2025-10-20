3cfd2ac5cb798c1220a27c0d1f873f3da0aeca88
Amosse Edouard <amosse.edouard@chronolink.fr>
2025-10-20 18:28:50 +0200

    Initial commit

---
+++ app/src/main/java/com/android/one/Lesson0.kt
@@ -0,0 +1,12 @@
+package com.android.one
+
+fun main() {
+    println("👋 Welcome to the Kotlin Playground!")
+
+    TODO(
+        "First thing first, print \"Hello, Kotlin!\" to the console."
+    )
+
+    println("If you see this message, you're good to go.\n")
+}
+
